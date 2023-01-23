9e26a73cbce615a3fecda94a7e650fa4d4ffce23
gloautomation <45288360+gloautomation@users.noreply.github.com>
2023-01-23 20:36:23 +0530

    Commit 21

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./components/Pages/Home";
 import { About } from "./components/Pages/About";
 import { Blog } from "./components/Pages/Blog";
-import { Contact } from "./components/Pages/Contact";
+import { Contact } from "./components/Pages/Contact"																					;
 
 function App() {
   return (
