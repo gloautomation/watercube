01158b45ffc4da2187304726163b9e28e503f5e6
gloautomation <45288360+gloautomation@users.noreply.github.com>
2023-01-23 20:00:52 +0530

    Commit 17

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
 
 test('renders learn react link', () => {
   render(<App />);
